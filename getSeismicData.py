9f6b6633b2888ef5417646ca7a983547ec43b4dc
Sebastian Salazar <ss@macbookSeba.local>
2020-09-27 11:22:01 -0300

    important functionality and syntax changes

--- getSeismicData.py
+++ getSeismicData.py
@@ -1,7 +1,7 @@
 
 '''
 getSeismicData.py: Extract all data from Centro Sismologico Nacional, UCH,
-    and save it into multiple folders sorted by year and month, and finally into a daily csv.
+    and save it into multiple folders sorted by year and month, and a finally into a daily csv.
 
 For further research in AI using ML to hopefully predict eathquakes and save lives.
 
@@ -13,71 +13,64 @@ import csv
 import requests
 import re
 import time
-import os
 import datetime
 
 now = datetime.datetime.now()
 
 #To get all data available since 2008 (the first record) until today
 years = [*range(2008, now.year + 1)]
-months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+months = [*range(1, 13)]
+days = [*range(1, 32)]
 
 
 for year in years:
     for month in months:
         for day in days:
-            try:
-                url = 'http://sismologia.cl/events/listados/' + str(year) + '/' + str(month).zfill(2) \
-                    + '/' +  str(year) + str(month).zfill(2) + str(day).zfill(2)  + '.html'
 
-                r = requests.get(url)
+            url = 'http://sismologia.cl/events/listados/' + str(year) + '/' + str(month).zfill(2) + '/' +  str(year) + str(month).zfill(2) + str(day).zfill(2)  + '.html'
 
-                #to check the status of the request
-                print(r)
+            r = requests.get(url)
 
-                if '<table>' in r.text:
+            #to check the status of the request
+            print(r)
 
-                    #to get table header 
-                    headerRe = re.findall(('<th>'
-                                            '(.*?)</th></tr>'),
-                                                r.text, re.DOTALL
-                                                )
 
-                    header = headerRe[0].split("</th><th>")
+            if '<table>' in r.text:
 
-                    #to get daily seismic data
-                    dailySeismsRe = re.findall(('target="centro">'
-                                            '(.*?)</td></tr>'),
-                                                r.text, re.DOTALL
-                                                )
+                headerRe = re.findall(('<th>'
+                            '(.*?)</th></tr>'),
+                                r.text, re.DOTALL
+                                )
 
-                    #for using multiple splits
-                    dailySeisms = [re.split('</a></td><td>|</td><td>', line) for line in dailySeismsRe]
+                header = headerRe[0].split("</th><th>")
 
-                    #...path to where to store the data
+                #to get daily seismic data
+                dailySeismsRe = re.findall(('target="centro">'
+                                        '(.*?)</td></tr>'),
+                                            r.text, re.DOTALL
+                                            )
 
-                    pathToDataFolder = ""
-                    pathToSave = pathToDataFolder + '/' + str(year) +'/'+ str(month) 
+                #for using multiple splits
+                dailySeisms = [re.split('</a></td><td>|</td><td>', line) for line in dailySeismsRe]
 
-                    if os.path.exists(pathToSave):
-                        fileName = 'day_' + str(day) + '.csv'
+                #...path to where to store the data
+
+                pathToDataFolder = ''
+                pathToSave = pathToDataFolder + '/' + str(year) +'/'+ str(month) 
+
+                if os.path.exists(pathToSave):
+                    fileName = 'day_' + str(day) + '.csv'
+                    for seism in dailySeisms:
                         with open(pathToSave + '/' + fileName, 'w', newline='') as file:
                             writer = csv.writer(file)
                             writer.writerow(header)
-                            for seism in dailySeisms:
-                                    writer.writerows(dailySeisms)
-                    else:
-                        #makedirs make all intermediate directories
-                        os.makedirs(pathToSave)
-                        fileName = 'day_' + str(day) + '.csv'
+                            writer.writerows(dailySeisms)
+                else:
+                    #makedirs make all intermediate directories
+                    os.makedirs(pathToSave)
+                    fileName = 'day_' + str(day) + '.csv'
+                    for seism in dailySeisms:
                         with open(pathToSave + '/' + fileName, 'w', newline='') as file:
                             writer = csv.writer(file)
                             writer.writerow(header)
-                            for seism in dailySeisms:
-                                    writer.writerows(dailySeisms)
-            except:
-                print("Error: No data for date " + str(day) + '/' + str(month) + '/' + str(year))
-                #Date format used is day/month/year
-
-
+                            writer.writerows(dailySeisms)
