81f745fa81cc76116edc87d0d722e13e4edb4350
Sebastian Salazar <ss@macbookSeba.local>
2020-09-27 10:21:51 -0300

    modified years list

--- getSeismicData.py
+++ getSeismicData.py
@@ -19,7 +19,7 @@ import datetime
 now = datetime.datetime.now()
 
 #To get all data available since 2008 (the first record) until today
-years = [year for year in range(2008, now.year + 1)]
+years = [*range(2008, now.year + 1)]
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 
